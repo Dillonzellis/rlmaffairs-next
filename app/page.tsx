17b17a884fcf7784dd4d40fe2e6f9118e2ba72af
dillonzellis <dillonzellis@gmail.com>
2023-11-10 14:59:47 -0500

    rough hero placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import { Welcome } from "@/components/pages/home/Welcome";
+
 export default function Home() {
   return (
     <main>
-      <h1>yeppers</h1>
+      <div className="py-4">
+        <Image
+          className="mx-auto"
+          src="/hero-vid-placeholder.png"
+          alt=""
+          width={1207}
+          height={688}
+        />
+      </div>
+      <Welcome />
     </main>
-  )
+  );
 }
