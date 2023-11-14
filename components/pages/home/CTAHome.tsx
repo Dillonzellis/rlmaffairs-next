f87de489f5268288fd07b0a1fffa9d4de479478e
dillonzellis <dillonzellis@gmail.com>
2023-11-14 00:19:16 -0500

    add rough formatting to CTAHome

--- components/pages/home/CTAHome.tsx
+++ components/pages/home/CTAHome.tsx
@@ -7,12 +7,15 @@ export const CTAHome = () => {
   return (
     <Section>
       <Container>
-        <Heading level="h2">Elevate Your Experience</Heading>
-        <BodyText>
-          Discover a realm where intricate design meets natural beauty. Every
-          corner of RLM Affairs is meticulously crafted, ensuring your event is
-          nothing short of extraordinary.
-        </BodyText>
+        <div className="mx-auto space-y-4 text-center">
+          <Heading level="h2">Elevate Your Experience</Heading>
+          <div>hr</div>
+          <BodyText className="mx-auto max-w-lg">
+            Discover a realm where intricate design meets natural beauty. Every
+            corner of RLM Affairs is meticulously crafted, ensuring your event
+            is nothing short of extraordinary.
+          </BodyText>
+        </div>
       </Container>
     </Section>
   );
