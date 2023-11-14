bdb929ec297cea21cc3681f21f737cfe42c271fa
dillonzellis <dillonzellis@gmail.com>
2023-11-13 21:39:26 -0500

    rough small feats section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Welcome } from "@/components/pages/home/Welcome";
+import { SmallFeats } from "@/components/pages/home/SmallFeats";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         />
       </div>
       <Welcome />
+      <SmallFeats />
     </main>
   );
 }
---
+++ components/pages/home/SmallFeats.tsx
@@ -0,0 +1,42 @@
+import { Container } from "@/components/Container";
+import { Section } from "@/components/ui/Section";
+import Image from "next/image";
+import { BsArrowRight } from "react-icons/bs";
+
+const Feat = () => {
+  return (
+    <div className="relative">
+      <div className="absolute left-8 top-8 rounded-xl bg-brandingGreen-400 p-4 text-white opacity-80">
+        <div>
+          <div className="text-lg">Visit us in Gainesville</div>
+          <div className="flex items-center gap-2 text-xl">
+            Book a Tour{" "}
+            <span>
+              <BsArrowRight className="h-5 w-5" />
+            </span>
+          </div>
+        </div>
+      </div>
+      <Image
+        src="/venue-feature-sm.png"
+        alt=""
+        height={446}
+        width={556}
+        className="rounded-lg shadow-lg"
+      />
+    </div>
+  );
+};
+
+export const SmallFeats = () => {
+  return (
+    <Section>
+      <Container>
+        <div className="flex justify-center gap-8">
+          <Feat />
+          <Feat />
+        </div>
+      </Container>
+    </Section>
+  );
+};
