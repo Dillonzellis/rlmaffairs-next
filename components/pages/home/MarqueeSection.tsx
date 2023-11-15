20d91f1094610608670a76cfff8e4639d7316c83
dillonzellis <dillonzellis@gmail.com>
2023-11-15 12:23:06 -0500

    rough marquee section

--- components/pages/home/MarqueeSection.tsx
+++ components/pages/home/MarqueeSection.tsx
@@ -1,9 +1,38 @@
 import { Section } from "@/components/ui/Section";
+import { awards } from "@/lib/data/marqueeAwards";
+import Image from "next/image";
+import Marquee from "react-fast-marquee";
+
+//TODO: img quality
+//TODO add award imgs
 
 export const MarqueeSection = () => {
   return (
     <Section>
-      <div>Marquee</div>
+      <div className="mx-auto max-w-[800px] text-center text-3xl capitalize">
+        were proud to partner with leading organizations and receive top
+        industry accolades
+      </div>
+      <Image
+        src="/leaves-1.png"
+        alt=""
+        width={140}
+        height={88}
+        className="mx-auto"
+      />
+      <Marquee pauseOnHover pauseOnClick>
+        <div className="flex justify-center gap-8">
+          {awards.map((award, idx) => (
+            <Image
+              key={idx}
+              src={award.src}
+              alt={award.alt}
+              width={100}
+              height={100}
+            />
+          ))}
+        </div>
+      </Marquee>
     </Section>
   );
 };
---
+++ lib/data/marqueeAwards.ts
@@ -0,0 +1,11 @@
+export const awards = [
+  { src: "/knot-2020.png", alt: "" },
+  { src: "/knot2021.png", alt: "" },
+  { src: "/knot2022.webp", alt: "" },
+  { src: "/wedding-wire-2023.png", alt: "" },
+
+  { src: "/knot-2020.png", alt: "" },
+  { src: "/knot2021.png", alt: "" },
+  { src: "/knot2022.webp", alt: "" },
+  { src: "/wedding-wire-2023.png", alt: "" },
+];
