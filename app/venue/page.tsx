559893bd0957203211248f5787dd05faec4ee85b
dillonzellis <dillonzellis@gmail.com>
2023-11-21 16:08:57 -0500

    create rough venue hero

--- app/venue/page.tsx
+++ app/venue/page.tsx
@@ -1,3 +1,17 @@
+import { Container } from "@/components/Container";
+import { Section } from "@/components/ui/Section";
+
 export default function VenuePage() {
-  return <main>Venue Page</main>;
+  return (
+    <main>
+      <Section>
+        <Container>
+          <div className="grid grid-cols-12">
+            <div className="col-span-4">text</div>
+            <div className="col-span-8">Image</div>
+          </div>
+        </Container>
+      </Section>
+    </main>
+  );
 }
