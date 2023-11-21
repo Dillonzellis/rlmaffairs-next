3bbfb225b639b6c6cd1727ec9657da5cfdb2fda4
dillonzellis <dillonzellis@gmail.com>
2023-11-21 16:08:48 -0500

    update nav links paths

--- lib/data/navLinks.ts
+++ lib/data/navLinks.ts
@@ -7,9 +7,9 @@ export type NavLinkProps = {
 type LinkItem = NavLinkProps;
 
 export const navLinks: LinkItem[] = [
-  { href: "/", label: "venue" },
-  { href: "/", label: "catering" },
-  { isLogo: true, href: "/logo-link", label: "RLM Logo" },
-  { href: "/", label: "florals" },
-  { href: "/", label: "our story" },
+  { href: "/venue", label: "venue" },
+  { href: "/catering", label: "catering" },
+  { isLogo: true, href: "/", label: "RLM Logo" },
+  { href: "/florals", label: "florals" },
+  { href: "/our-story", label: "our story" },
 ];
